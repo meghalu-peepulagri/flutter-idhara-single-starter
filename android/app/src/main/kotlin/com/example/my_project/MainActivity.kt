fffc2ff20bd80507428a92c48da4def6941f5721
Muninaik-mude <munikumar.m@orotron.com>
2025-12-11 15:19:37 +0530

    delete files

--- android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.mycompany.idhara
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/index.dart
+++
@@ -1,7 +0,0 @@
-// Export pages
-export '/pages/dashboard/dashboard_widget.dart' show DashboardWidget;
-export '/motor_control/motor_control_widget.dart' show MotorControlWidget;
-export '/devices/devices_widget.dart' show DevicesWidget;
-export '/add_devices/add_devices_widget.dart' show AddDevicesWidget;
-export '/q_r_code/q_r_code_widget.dart' show QRCodeWidget;
-export '/locations/locations_widget.dart' show LocationsWidget;
